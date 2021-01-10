7fad030e47677e6f888185aa66f4465c2bc1bf24
MagicAtom <1263750383@qq.com>
2021-01-10 10:28:35 +0800

    Remove build message.

--- opengl_helper/assimp/revision.h
+++ opengl_helper/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x9405713e
+#define GitVersion 0x8d35e024
 #define GitBranch "dev"
 
 #define VER_MAJOR 5
@@ -16,7 +16,7 @@
 #if (GitVersion == 0)
 #define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD)
 #else
-#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 9405713e)"
+#define VER_FILEVERSION_STR         STR(VER_MAJOR) "." STR(VER_MINOR) "." STR(VER_PATCH) "." STR(VER_BUILD) " (Commit 8d35e024)"
 #endif
 
 #ifdef  NDEBUG
